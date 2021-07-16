85fdb97db15be798228a9e135622b6907deb1cef
lamafab <42901763+lamafab@users.noreply.github.com>
2021-07-16 19:33:21 +0200

    read and parse content from file

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -1,15 +1,39 @@
+use proc_macro::{TokenStream, TokenTree};
+use project_x_metadata::{parse_jsonrpc_metadata, MetadataVersion};
+use std::fs::read_to_string;
+
 #[proc_macro_attribute]
-pub fn from_file(
-    args: proc_macro::TokenStream,
-    _: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-    unimplemented!()
+pub fn from_file(args: TokenStream, _: TokenStream) -> TokenStream {
+    // Extract path.
+    let tree = args
+        .into_iter()
+        .nth(0)
+        .expect("Expected path literal as argument. E.g \"/path/to/file\"");
+
+    let path = match tree {
+        TokenTree::Literal(path) => path.to_string(),
+        _ => panic!("Expected path literal as argument. E.g \"/path/to/file\""),
+    };
+
+    // Read content from file.
+    let content = read_to_string(&path).expect(&format!(
+        "Failed to read runtime metadata from \"{}\"",
+        path
+    ));
+
+    // Parse runtime metadata
+    let metadata = parse_jsonrpc_metadata(content)
+        .map_err(|err| panic!("Failed to parse runtime metadata: {:?}", err))
+        .unwrap();
+
+    process_runtime_metadata(metadata)
 }
 
 #[proc_macro_attribute]
-pub fn from_web(
-    args: proc_macro::TokenStream,
-    _: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
+pub fn from_web(args: TokenStream, _: TokenStream) -> TokenStream {
+    unimplemented!()
+}
+
+fn process_runtime_metadata(metadata: MetadataVersion) -> TokenStream {
     unimplemented!()
 }

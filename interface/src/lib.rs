88eca791d9a71cd1b78d3c1f3ef5a943ecc640aa
lamafab <42901763+lamafab@users.noreply.github.com>
2021-07-20 16:50:58 +0200

    implement PolkadotSigner

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate parity_scale_codec;
+use parity_scale_codec::{Decode, Encode};
 
 pub use latest::*;
 
@@ -26,12 +25,67 @@ pub mod common {
     }
 }
 
-pub type PolkadotSignedExtrinsic =
-    SignedExtrinsic<MultiAddress<AccountId32, ()>, MultiSignature, Extra>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub enum Error {
+    BuilderError(String),
+}
+
+pub type PolkadotSignedExtrinsic<Call> =
+    SignedExtrinsic<MultiAddress<AccountId32, ()>, Call, MultiSignature, Extra>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PolkadotSignerBuilder<Call> {
+    signer: Option<MultiSigner>,
+    call: Option<Call>,
+}
+
+impl<Call: Encode> PolkadotSignerBuilder<Call> {
+    pub fn new() -> Self {
+        Self {
+            signer: None,
+            call: None,
+        }
+    }
+    pub fn set_signer(self, signer: MultiSigner) -> Self {
+        Self {
+            signer: Some(signer),
+            ..self
+        }
+    }
+    pub fn set_call(self, call: Call) -> Self {
+        Self {
+            call: Some(call),
+            ..self
+        }
+    }
+    pub fn build(self) -> Result<PolkadotSignedExtrinsic<Call>> {
+        let signer = self
+            .signer
+            .ok_or(Error::BuilderError("set_signer".to_string()))?;
+        let call = self
+            .call
+            .ok_or(Error::BuilderError("set_call".to_string()))?;
+
+        // TODO:
+        let sig = match signer {
+            MultiSigner::Ed25519(_) => MultiSignature::Ed25519(Sig),
+            MultiSigner::Sr25519(_) => MultiSignature::Sr25519(Sig),
+            MultiSigner::Ecdsa(_) => MultiSignature::Ecdsa(Sig),
+        };
+
+        let addr = signer.into();
+
+        Ok(SignedExtrinsic {
+            signature: Some((addr, sig, Extra)),
+            function: call,
+        })
+    }
+}
 
 /// The signed extrinsic, aka. "UncheckedExtrinsic" in terms of substrate.
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
-pub struct SignedExtrinsic<Address, Signature, Extra> {
+pub struct SignedExtrinsic<Address, Call, Signature, Extra> {
     pub signature: Option<(Address, Signature, Extra)>,
     pub function: Call,
 }
@@ -52,8 +106,14 @@ pub enum MultiSignature {
     Ecdsa(Sig),
 }
 
+impl From<MultiSigner> for MultiAddress<AccountId32, ()> {
+    fn from(_signer: MultiSigner) -> Self {
+        unimplemented!()
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
-pub struct Sig {}
+pub struct Sig;
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
 pub enum MultiSigner {
@@ -63,13 +123,10 @@ pub enum MultiSigner {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
-pub struct Public {}
+pub struct Public;
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
 pub struct AccountId32([u8; 32]);
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
-pub struct Extra {}
-
-#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
-pub struct Call {}
+pub struct Extra;

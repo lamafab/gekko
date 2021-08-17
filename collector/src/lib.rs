d6f899b8943fe7d484089c1d9b29762efc1747f6
lamafab <42901763+lamafab@users.noreply.github.com>
2021-08-17 22:27:03 +0200

    create event loop for collector

--- collector/src/lib.rs
+++ collector/src/lib.rs
@@ -1,5 +1,9 @@
 use reqwest::Client;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use tokio::time::{sleep, Duration};
+
+const BLOCK_HASH_LIMIT: u64 = 50;
+const TIMEOUT: u64 = 10;
 
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
@@ -8,11 +12,11 @@ pub struct RpcRequest<'a, T> {
     pub id: i64,
     pub jsonrpc: &'a str,
     pub method: &'a str,
-    pub params: Vec<T>,
+    pub params: T,
 }
 
 impl<'a, T> RpcRequest<'a, T> {
-    fn new(method: RpcMethod, params: Vec<T>) -> Self {
+    fn new(method: RpcMethod, params: T) -> Self {
         RpcRequest {
             id: 1,
             jsonrpc: "2.0",
@@ -23,15 +27,15 @@ impl<'a, T> RpcRequest<'a, T> {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct RpcResponse {
+pub struct RpcResponse<T> {
     pub jsonrpc: String,
-    pub result: Vec<String>,
+    pub result: T,
     pub id: i64,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum RpcMethod {
-    Block,
+    Header,
     BlockHash,
     RuntimeVersion,
     Metadata,
@@ -40,7 +44,7 @@ enum RpcMethod {
 impl RpcMethod {
     fn as_str(&self) -> &'static str {
         match self {
-            Block => "chain_getBlock",
+            Header => "chain_getHeader",
             BlockHash => "chain_getBlockHash",
             RuntimeVersion => "state_getRuntimeVersion",
             Metadata => "state_getMetadata",
@@ -48,15 +52,75 @@ impl RpcMethod {
     }
 }
 
-pub fn run() {}
+pub async fn local() -> Result<()> {
+    let mut latest = latest_block().await?;
 
-async fn get<B: Serialize, R: DeserializeOwned>(method: RpcMethod, body: Vec<B>) -> Result<R> {
+    let mut current_block = 0;
+    loop {
+        let to = latest.min(BLOCK_HASH_LIMIT);
+        let range = (current_block..=to).collect();
+
+        let header = get::<Vec<u64>, Vec<String>>(RpcMethod::BlockHash, range).await?;
+        let version = get::<(), RuntimeVersion>(RpcMethod::RuntimeVersion, ()).await?;
+
+        current_block = to + 1;
+
+        if latest < current_block {
+            sleep(Duration::from_secs(TIMEOUT)).await;
+            latest = latest_block().await?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Convenience function for fetching the latest block number.
+async fn latest_block() -> Result<u64> {
+    get::<Option<()>, Header>(RpcMethod::Header, None)
+        .await?
+        .number
+        .parse()
+        .map_err(|err| anyhow::Error::from(err))
+}
+
+/// Response when calling `chain_getHeader`.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Header {
+    pub digest: serde_json::Value,
+    #[serde(rename = "extrinsicsRoot")]
+    pub extrinsics_root: String,
+    pub number: String,
+    #[serde(rename = "parentHash")]
+    pub parent_hash: String,
+    #[serde(rename = "stateRoot")]
+    pub state_root: String,
+}
+
+/// Response when calling `state_getRuntimeVersion`.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RuntimeVersion {
+    pub apis: Vec<(String, i64)>,
+    #[serde(rename = "authoringVersion")]
+    pub authoring_version: i64,
+    #[serde(rename = "implName")]
+    pub impl_name: String,
+    #[serde(rename = "implVersion")]
+    pub impl_version: i64,
+    #[serde(rename = "specName")]
+    pub spec_name: String,
+    #[serde(rename = "specVersion")]
+    pub spec_version: i64,
+}
+
+/// Convenience function for executing a RPC call.
+async fn get<B: Serialize, R: DeserializeOwned>(method: RpcMethod, body: B) -> Result<R> {
     Client::new()
         .post(method.as_str())
         .json(&RpcRequest::new(method, body))
         .send()
         .await?
-        .json::<R>()
+        .json::<RpcResponse<R>>()
         .await
+        .map(|res| res.result)
         .map_err(|err| err.into())
 }
